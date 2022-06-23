f01a190472b2be81b2399faa6ea0e7cdef7bb58c
RoRomario360 <99145947+RoRomario360@users.noreply.github.com>
2022-06-23 21:06:21 +0000

    Deploying to gh-pages from @ RoRomario360/goit-js-hw-09@3e274d971f24f672f4b252230152f1da71144d4c 🚀

--- 01-color-switcher.a6ab8a1f.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start"),d=document.querySelector("button[data-stop");let o=0;e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(o),d.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.a6ab8a1f.js.map
