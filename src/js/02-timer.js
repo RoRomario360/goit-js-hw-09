6092ee322b1b7acfdd1d0163c147faae3d6bc27d
RoRomario360 <fotoromario@gmail.com>
2022-06-23 17:50:33 +0300

    task-2-completed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,86 @@
-const timer = {
-  start() {
-    const startTime = Date.now();
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const timerComponents = getTimeComponents(deltaTime);
-      console.log(timerComponents);
-    }, 1000);
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+//
+const inputDate = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const day = document.querySelector('[data-days]');
+const hour = document.querySelector('[data-hours]');
+const minute = document.querySelector('[data-minutes]');
+const second = document.querySelector('[data-seconds]');
+let interval = 0;
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    // console.log(selectedDates[0]);
+    if (selectedDates[0] <= Date.now()) {
+      alert('Please choose a date in the future');
+      btnStart.disabled = true;
+    } else {
+      btnStart.disabled = false;
+    }
   },
-};
-// timer.start();
+});
 
-//Function from Repeta
-function getTimeComponents(time) {
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+btnStart.addEventListener('click', onClickStart);
 
-  return { hours, mins, secs };
+function onClickStart(event) {
+  timerId = setInterval(() => {
+    const choosenDate = new Date(inputDate.value);
+    const timeToFinish = choosenDate - Date.now();
+
+    let { days, hours, minutes, seconds } = convertMs(timeToFinish);
+
+    day.textContent = days < 10 ? '0' + days : days;
+    hour.textContent = hours < 10 ? '0' + hours : hours;
+    minute.textContent = minutes < 10 ? '0' + minutes : minutes;
+    second.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+    if (timeToFinish < 1000) {
+      clearInterval(timerId);
+    }
+  }, 1000);
 }
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+// const timer = {
+//   start() {
+//     const startTime = Date.now();
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = currentTime - startTime;
+//       const timerComponents = getTimeComponents(deltaTime);
+//       console.log(timerComponents);
+//     }, 1000);
+//   },
+// };
+// // timer.start();
+
+// //Function from Repeta
+// function getTimeComponents(time) {
+//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+//   const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+//   return { hours, mins, secs };
+// }
