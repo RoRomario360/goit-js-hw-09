70dc1e93cb3c0e71524f7f29d60ca02dfd0f2827
RoRomario360 <fotoromario@gmail.com>
2022-06-23 12:31:52 +0300

    task-1-completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start');
+const btnStop = document.querySelector('button[data-stop');
+let intervalId = 0;
+btnStart.addEventListener('click', onclickBtnStart);
+btnStop.addEventListener('click', onclickBtnStop);
 
+function onclickBtnStart() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  if ((btnStart.disabled = true)) {
+    btnStop.disabled = false;
+  }
+}
+
+function onclickBtnStop() {
+  clearInterval(intervalId);
+  if ((btnStop.disabled = true)) {
+    btnStart.disabled = false;
+  }
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
