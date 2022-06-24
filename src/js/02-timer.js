dcae594d22f113d5b4b7bdc88010c700697d5c66
RoRomario360 <fotoromario@gmail.com>
2022-06-24 15:53:05 +0300

    task-2-corr

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-//
-const inputDate = document.querySelector('#datetime-picker');
-const btnStart = document.querySelector('[data-start]');
-const day = document.querySelector('[data-days]');
-const hour = document.querySelector('[data-hours]');
-const minute = document.querySelector('[data-minutes]');
-const second = document.querySelector('[data-seconds]');
-let interval = 0;
-btnStart.disabled = true;
 
 Notiflix.Notify.init({
   width: '300px',
@@ -17,32 +8,44 @@ Notiflix.Notify.init({
   closeButton: false,
 });
 
-flatpickr('#datetime-picker', {
+const date = document.querySelector('#datetime-picker');
+const button = document.querySelector('[data-start]');
+const day = document.querySelector('[data-days]');
+const hour = document.querySelector('[data-hours]');
+const minute = document.querySelector('[data-minutes]');
+const second = document.querySelector('[data-seconds]');
+
+let timerId = null;
+
+button.disabled = true;
+
+flatpickr(date, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
     if (selectedDates[0] <= Date.now()) {
-      Notiflix.Notify.failure('Hey Bro, please enter valid date!!!');
-      // alert('Please choose a date in the future');
-      btnStart.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
     } else {
-      btnStart.disabled = false;
+      button.disabled = false;
+
+      // console.log(selectedDates[0]);
     }
   },
 });
 
-btnStart.addEventListener('click', onClickStart);
+button.addEventListener('click', onClickStart);
 
-function onClickStart(event) {
+function onClickStart() {
   timerId = setInterval(() => {
-    const choosenDate = new Date(inputDate.value);
+    const choosenDate = new Date(date.value);
     const timeToFinish = choosenDate - Date.now();
 
+    button.disabled = true;
+
     let { days, hours, minutes, seconds } = convertMs(timeToFinish);
-    btnStart.disabled = true;
+
     day.textContent = days < 10 ? '0' + days : days;
     hour.textContent = hours < 10 ? '0' + hours : hours;
     minute.textContent = minutes < 10 ? '0' + minutes : minutes;
@@ -50,7 +53,8 @@ function onClickStart(event) {
 
     if (timeToFinish < 1000) {
       clearInterval(timerId);
-      btnStart.disabled = true;
+
+      button.disabled = true;
     }
   }, 1000);
 }
@@ -73,27 +77,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-//lib
-
-// const timer = {
-//   start() {
-//     const startTime = Date.now();
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startTime;
-//       const timerComponents = getTimeComponents(deltaTime);
-//       console.log(timerComponents);
-//     }, 1000);
-//   },
-// };
-// // timer.start();
-
-// //Function from Repeta
-// function getTimeComponents(time) {
-//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-//   return { hours, mins, secs };
-// }
